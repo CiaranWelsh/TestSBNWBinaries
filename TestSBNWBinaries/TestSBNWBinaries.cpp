ede2f0cb3f52a634aabf967279055074cc1eac2d
CiaranWelsh <c.welsh2@newcastle.ac.uk>
2020-01-22 14:35:58 -0800

    first commit

---
+++ TestSBNWBinaries/TestSBNWBinaries.cpp
@@ -0,0 +1,48 @@
+﻿// TestSBNWBinaries.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include "TestSBNWBinaries.h"
+#include "windows.h"
+
+using namespace std;
+
+typedef int(_cdecl *DLLPROC)(LPWSTR);
+
+HINSTANCE loadDLL(LPCSTR name) {
+	HINSTANCE hinstDLL = LoadLibrary(name);
+	if (!hinstDLL) {
+		std::cout << "could not load the dynamic library" << std::endl;
+		exit(1);
+	}
+	return hinstDLL;
+}
+
+DLLPROC loadFunc(HINSTANCE hInstance, LPCSTR name) {
+	DLLPROC proc = (DLLPROC)GetProcAddress(hInstance, name);
+	if (proc == NULL) {
+		std::cout << "Could not get function name: " << name << endl;
+	}
+	return proc;
+}
+
+
+
+int main()
+{
+	LPCSTR path= "D:\\TestSBNWBinaries\\libs\\sbml_draw.dll";
+	HINSTANCE libsbmlDraw= loadDLL(path);
+
+	// This is now a function pointer
+	DLLPROC gf_SBMLModel = loadFunc(libsbmlDraw, "gf_SBMLModel");
+
+	std::string model_path = "D:\\TestSBNWBinaries\\TestSBNWBinaries\\network.sbml";
+
+
+	(gf_SBMLModel);
+
+	FreeLibrary(libsbmlDraw);
+
+	
+	return 0;
+}
---
+++ include/sbnw/SagittariusCore.h
@@ -0,0 +1,30 @@
+/* MIT License
+ */
+
+//== FILEDOC =========================================================================
+
+/** @file SagittariusCore.h
+ * @brief First file included
+  * @details This file is a wrapper to other headers that provide necessary
+  * core capabilities required by the source. It is always included first
+  * in library source files.
+  */
+
+//== BEGINNING OF CODE ===============================================================
+
+#ifndef __SAGITTARIUS__SAGITTARIUS_CORE__H_
+#define __SAGITTARIUS__SAGITTARIUS_CORE__H_
+
+//== INCLUDES ========================================================================
+
+#include "SagittariusConfigure.h"
+#include "SagittariusCommon.h"
+#include "SagittariusAssert.h"
+#include "BaseWORDs.h"
+#ifdef __cplusplus
+    #include "SagittariusException.h"
+#endif
+
+
+
+#endif
